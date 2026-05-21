df3b8219d9624db5c52f1b648082f0624fb8b883
HariHarshh <hariharshhthakur@gmail.com>
2026-05-21 16:32:16 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,12 @@
+name = "HARSHH"
+age = 13
+weight = 40.50
+
+print("\n After Type Casting.....")
+age = str(age)
+print(age)
+print("Data type of age is", type (age))
+
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is" , type(weight))
